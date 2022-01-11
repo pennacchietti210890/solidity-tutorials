2cbf8419404916b4407d03617c2122866b847361
Mattia Pennacchietti <mattia.pennacchietti@revolut.com>
2022-01-11 09:17:27 +0000

    deploying, compile, testing scripts

--- deploy.js
+++ deploy.js
@@ -1,1 +1,33 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'step trust twist dinner april oil involve antenna enjoy hello alert museum', 
+    'https://rinkeby.infura.io/v3/5b84946db2454b80ae466cdf94a8c0dc'
+);
+
+const web3 = new Web3(provider);
+
+// creating a funct so we can use async
+const deploy = async () => {
+    // get a list of all accounts associated with mnemonic above
+    const accounts = await web3.eth.getAccounts();        
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    // deploying
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        // this is constructing the contract object
+        .deploy({ 
+            data: bytecode, 
+            arguments: ['Hi there!']})
+        // deploying is happening here
+        .send({ from: accounts[0], gas: '1000000'});
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+
+};
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,43 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+const INITIAL_STRING = 'Hi there!'
+
+beforeEach(async ()=>{ 
+    // get a list of all accounts 
+    accounts = await web3.eth.getAccounts();        
+
+    // use one of those accounts to deploy the contract
+    // need the contract bytecode
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        // this is constructing the contract object
+        .deploy({ 
+            data: bytecode, 
+            arguments: [INITIAL_STRING]})
+        // deploying is happening here
+        .send({ from: accounts[0], gas: '1000000'});
+});
+
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
+    });
+});
